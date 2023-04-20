ddca9dba0dccbf71adfe88434c0388dae6f849ab
Vickyb94 <115244751+Vickyb94@users.noreply.github.com>
2023-04-20 10:16:13 -0300

    Update queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,15 +1,103 @@
 import { gql } from '@apollo/client';
 
 export const All_Users = gql`
-query users {
-  users {
-    id
-    userName
-    email
-    password
+query Users {
+    users {
+      id
+      userName
+      email
+      password
+      pendingEvents {
+        _id
+        name
+        description
+        startDate
+        endDate
+        type
+        isPrivate
+        location
+        createdBy {
+          id
+          userName
+          email
+          password
+        }
+        attendees {
+          id
+          userName
+          email
+          password
+        }
+        invitees {
+          id
+          userName
+          email
+          password
+        }
+      }
+      acceptedEvents {
+        _id
+        name
+        description
+        startDate
+        endDate
+        type
+        isPrivate
+        location
+      }
+      tasks {
+        id
+        title
+        description
+        assignedTo {
+          id
+          userName
+          email
+          password
+        }
+        deadline
+        completed
+        event {
+          _id
+          name
+          description
+          startDate
+          endDate
+          type
+          isPrivate
+          location
+        }
+      }
+    }
   }
-}
-`;
+  `;
+
+  export const EVENT_BY_ID = gql`
+  query event($eventId: ID!) {
+    event(id: $eventId) {
+      _id
+      name
+      description
+      startDate
+      endDate
+      type
+      isPrivate
+      location
+      invitees {
+        userName
+        id
+      }
+      createdBy {
+        userName
+        id
+      }
+      attendees {
+        userName
+        id
+      }
+    }
+  }
+  `;
 
 export const LOGGED_IN_USER = gql`
 query LoggedinUser($userId: ID!) {
